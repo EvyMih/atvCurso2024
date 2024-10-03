2a2c8898ce8c2de8fe71c83bece6fc810977ce40
aluno.lab <aluno.lab@ides.lab>
2024-10-03 17:10:21 -0300

    up

--- contaBanco/contaBanco.js
+++ contaBanco/contaBanco.js
@@ -1,65 +1,67 @@
+function banco(){
     //testegit
     var saldo = Number(prompt("Digite seu saldo: "))
     
-  /*   var saldoFinal = saldo;
+    /*   var saldoFinal = saldo;
     let saldoAtual = 0;. */
-
-do{ 
-    var opc = Number(prompt("Escolha como proseguir: \n1 - Saque\n2 - Depósito\n3 - Transferência"))
-
-    switch(opc){
-        case 1:
-            var valorSaque = Number(prompt("Digite o valor do saque: "))
     
-            if(valorSaque > saldo){
-                alert("Saldo insuficiente")
-            }else{
+    do{ 
+        var opc = Number(prompt("Escolha como proseguir: \n1 - Saque\n2 - Depósito\n3 - Transferência"))
+    
+        switch(opc){
+            case 1:
+                var valorSaque = Number(prompt("Digite o valor do saque: "))
+        
+                if(valorSaque > saldo){
+                    alert("Saldo insuficiente")
+                }else{
+                    
+                    alert(`Saque efetuado com sucesso.\nSeu saldo agora é R$ ${saldo - valorSaque}`)
+                    saldo += - valorSaque
+                }
+    
                 
-                alert(`Saque efetuado com sucesso.\nSeu saldo agora é R$ ${saldo - valorSaque}`)
-                saldo += - valorSaque
-            }
-
-            
-        break;
+            break;
+        
+            case 2:
+                var deposito = Number(prompt("Digite o valor do depósito: "))
+                alert(`Depósito efetuado com sucesso.\n Seu saldo agora é R$ ${saldo + deposito}`)
     
-        case 2:
-            var deposito = Number(prompt("Digite o valor do depósito: "))
-            alert(`Depósito efetuado com sucesso.\n Seu saldo agora é R$ ${saldo + deposito}`)
-
-
-            saldo += + deposito;
-        break;
     
-        case 3:
-            var transferência = Number(prompt("Escolha como proseguir: \n1 - PIX\n2 - TED"))
+                saldo += + deposito;
+            break;
+        
+            case 3:
+                var transferência = Number(prompt("Escolha como proseguir: \n1 - PIX\n2 - TED"))
+        
+                switch(transferência){
+                    case 1:
+                        var valorTransf = Number(prompt("Digite o valor da transferência: "))
+        
+                        if(valorTransf > saldo){
+                            alert("Saldo insuficiente")
+                        }else{
+                            alert(`Transferência efetuada com sucesso.\nSeu saldo agora é R$ ${saldo - valorTransf}`)
+                        }
+                    break;
+        
+                    case 2:
+                        var valorTransf = Number(prompt("Digite o valor da transferência: "))
+                        var taxa = 9.8
+        
+                        if((valorTransf + taxa) > saldo ){
+                            alert("Saldo insuficiente")
+                        }else{
+                            alert(`Transferência efetuada com sucesso.\nSeu saldo agora é R$ ${(saldo - valorTransf - taxa).toFixed(2)}`)
+                        }
+                }
+                saldo += - valorTransf;
+            break;  
+        }
+        var resposta = prompt("deseja continuar?\n1 - Sim\n2 - Não")
+        
     
-            switch(transferência){
-                case 1:
-                    var valorTransf = Number(prompt("Digite o valor da transferência: "))
+    }while(resposta != 2)
     
-                    if(valorTransf > saldo){
-                        alert("Saldo insuficiente")
-                    }else{
-                        alert(`Transferência efetuada com sucesso.\nSeu saldo agora é R$ ${saldo - valorTransf}`)
-                    }
-                break;
-    
-                case 2:
-                    var valorTransf = Number(prompt("Digite o valor da transferência: "))
-                    var taxa = 9.8
-    
-                    if((valorTransf + taxa) > saldo ){
-                        alert("Saldo insuficiente")
-                    }else{
-                        alert(`Transferência efetuada com sucesso.\nSeu saldo agora é R$ ${(saldo - valorTransf - taxa).toFixed(2)}`)
-                    }
-            }
-            saldo += - valorTransf;
-        break;  
-    }
-    var resposta = prompt("deseja continuar?\n1 - Sim\n2 - Não")
-   
-
-}while(resposta != 2)
-
-alert(`Seu saldo ficou em R$ ${saldo}`)
+    alert(`Seu saldo ficou em R$ ${saldo}`)
+}
